41ef9de24d884a19b44d485ffbdaf4f8f3c337d9
hispanos <mailermartinez@gmail.com>
2021-06-10 21:13:07 -0500

    Updated version

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //Importar sw-utils
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v2';
+const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
